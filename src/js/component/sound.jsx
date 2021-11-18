dae85e28861fdde178166c46acf55cc02c9f3cfe
Catsil <imgloriasilva@gmail.com>
2021-11-18 11:43:52 +0000

    completed

---
+++ src/js/component/sound.jsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from "react";
+
+const BASE_URL = "https://assets.breatheco.de/apis/sound/";
+
+const Sound = () => {
+	const [mapList, setMapList] = useState([]);
+	const [playSong, setPlaySong] = useState({});
+
+	useEffect(() => {
+		fetch(BASE_URL.concat("songs"), {
+			method: "GET"
+		})
+			.then(response => {
+				if (!response.ok) {
+					throw new Error(response.statusText);
+				}
+				return response.json();
+			})
+			.then(responseAsJson => {
+				setMapList(responseAsJson);
+			})
+			.catch(error => {
+				console.log("Warning!! Try again.", error);
+			});
+	}, []);
+
+	let songPlaylist = mapList.map((tune, index) => {
+		return (
+			<li
+				onEnded={() => {
+					nextSong();
+				}}
+				{...tune.name}
+				className="songName"
+				key={index.toString()}
+				onClick={() => {
+					setPlaySong({
+						url: tune.url,
+						name: tune.name,
+						position: index
+					});
+				}}>
+				{tune.name}
+			</li>
+		);
+	});
+
+	useEffect(() => {
+		if (mapList.length > 0) {
+			setPlaySong({ ...mapList[0], position: 0 });
+		}
+	}, [mapList]);
+
+	const nextSong = () => {
+		let position = playSong.position + 1;
+		if (position < mapList.length) {
+			setPlaySong({
+				...mapList[position],
+				position: position
+			});
+		} else {
+			setPlaySong({
+				...mapList[0],
+				position: 0
+			});
+		}
+	};
+
+	const previousSong = () => {
+		let position = playSong.position - 1;
+		if (position < mapList.length) {
+			setPlaySong({
+				...mapList[position],
+				position: position
+			});
+		} else {
+			setPlaySong({
+				...mapList[0],
+				position: 0
+			});
+		}
+	};
+	// Pasamos sacar por pantalla el resultado de nuestro reproductor
+	return (
+		<fragment>
+			<div className="container">
+				<h1 className="Title">MUSIC PLAYER</h1>
+
+				<div className="row">
+					<div className="playlist col-6">
+						<ul>{songPlaylist}</ul>
+					</div>
+					<div className="console">
+						<div className="mysongs">{playSong.name}</div>
+
+						<audio
+							src={BASE_URL.concat(playSong.url)}
+							controls
+							onEnded={() => nextSong()}></audio>
+						<button
+							className="btn "
+							onClick={() => {
+								previousSong();
+							}}>
+							<i className="fas fa-backward"></i>
+						</button>
+						<button
+							className="btn"
+							offset-4
+							onClick={() => {
+								nextSong();
+							}}>
+							<i className="fas fa-forward"></i>
+						</button>
+					</div>
+				</div>
+			</div>
+		</fragment>
+	);
+};
+export default Sound;
